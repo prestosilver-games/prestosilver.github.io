f4bfb90a2376c86c4c015658fe5ba52541898e50
prestosilver <prestosilver.ptp@gmail.com>
2025-12-08 08:09:30 -0500

    fix

--- src/generators/list.zig
+++ src/generators/list.zig
@@ -91,13 +91,13 @@ pub fn gen(self: *const Self, dom: *rem.Dom) !*rem.Dom.Element {
                     try elems.append(link);
 
                     const page: PostPage = .init(try std.fmt.allocPrint(dom.allocator, "{s}/{s}", .{ self.folder, item.name }));
-                    const conts = try page.gen(dom);
+                    const page_conts = try page.gen(dom);
                     if (page.id == "")
                         return error.MissingPageId;
 
                     const page_url = try std.fmt.allocPrint(dom.allocator, "posts/{s}.html", .{page.id});
 
-                    try util.writePage(dom, try std.fs.cwd().openDir("root", .{}), conts);
+                    try util.writePage(dom, try std.fs.cwd().openDir("root", .{}), page_conts);
 
                     try link.appendAttribute(dom.allocator, .{ .prefix = .none, .namespace = .none, .local_name = "href" }, page_url);
                 }
